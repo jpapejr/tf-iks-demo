0f87c047ad416f4acefac7cd06ee2d2a22a7129a
jpapejr <jpapejr@icloud.com>
2020-10-19 15:45:39 -0400

    add outputs

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "manager_instance_ip" {
+    value = ibm_is_floating_ip.fip.address
+}
+
+output "cluster_endpoint" {
+    value = ibm_container_vpc_cluster.cluster.private_service_endpoint_url
+}
+
+output "clusterid" {
+    value = ibm_container_vpc_cluster.cluster.id
+}
