fe33ba56e7d31c255fea8d3fc71c5c0f3b8ffb57
jpapejr <jpapejr@icloud.com>
2020-10-19 16:16:38 -0400

    typo

--- compute.tf
+++ compute.tf
@@ -2,7 +2,7 @@ resource "ibm_is_instance" "inst1" {
   name = "${var.project}-${var.environment}-inst1"
   primary_network_interface {
     name = "eth0"
-    security_groups = [ ibm_is_vpc.vpc.default_security_group.id ]
+    security_groups = [ ibm_is_vpc.vpc.default_security_group ]
     subnet = ibm_is_subnet.subnet1.id
   }
   keys = [ data.ibm_is_ssh_key.key.id ]
