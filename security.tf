76569d9a0bd4fd334c3ac4b50eeb4e18e86f9eee
John Pape <jpapejr@icloud.com>
2020-10-28 14:55:44 -0400

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,3 +1,11 @@
-data "ibm_is_ssh_key" "key" {
-  name = "jtpape"
+resource "tls_private_key" "privatekey" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "ibm_is_ssh_key" "key" {
+  name = "${var.project}-${var.environment}-key"
+  resource_group = ibm_resource_group.group.id
+  public_key = tls_private_key.privatekey.public_key_openssh
+  depends_on = [tls_private_key.privatekey]
 }
